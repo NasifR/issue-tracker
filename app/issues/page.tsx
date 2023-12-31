40e83659114f55dac035cae9d0270eda77a96f0a
unknown <nasifjuhayer@gmail.com>
2023-12-30 19:06:45 -0500

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const issuesPage = () => {
   return (
-    <div>issuesPage</div>
+    <div>
+      <Button>New Issue</Button>
+    </div>
   )
 }
 
